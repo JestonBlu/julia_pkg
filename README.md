6a7cba643fe82aa25e783bb84ea82aa0cf5a4e0b
jestonblu <jestonblu@gmail.com>
2019-10-20 22:52:23 -0500

    basic package

---
+++ README.md
@@ -0,0 +1,1 @@
+# Example Julia Package
--- src/julia_pkg.jl
+++ src/julia_pkg.jl
@@ -1,5 +1,9 @@
 module julia_pkg
 
+import Random
+import JSON
+
 greet() = print("Hello World!")
+greet_alien() = print("Hello ", Random.randstring(8))
 
 end # module
